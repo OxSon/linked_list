ab60e1c81b9527d58dc2fe74d4186e1003a5beb6
OxSon <al3cmills@gmail.com>
2019-02-26 12:26:25 -0700

    Remove spaces between a pointer and it's field
    E.g. before: 'current -> next' after: 'current->next'

--- src/main.c
+++ src/main.c
@@ -8,14 +8,14 @@ struct node {
 
 /*
 void setItem(struct node *node, int item) {
-    node -> item = item;
+    node->item = item;
 }
 */
 
 /*
 void addNext(struct node *current, struct node next) {
-    if(current -> pnext != NULL) {
-        current -> pnext = &next;
+    if(current->pnext != NULL) {
+        current->pnext = &next;
     } else {
         //This doesn't behave how I expected it to
         #warning attempting to add 'next' to a node that already has next
@@ -27,42 +27,43 @@ int main() {
     //TODO get data from user
     int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, i = 0;
     //will this work
-    struct node *head = malloc(sizeof(struct node)), *current = head;
+    //struct node *head = malloc(sizeof(struct node)), *current = head;
     //or do we need this
-    //struct node *head = (struct node *) malloc(sizeof(struct node));
+    struct node *head = (struct node *) malloc(sizeof(struct node)),
+                *current = head;
 
     int data_length = sizeof(data) / sizeof(data[0]);
 
     //set up our list
     while(i < data_length) {
-        current -> item = data[i]; //set data for current node
+        current->item = data[i]; //set data for current node
         //make a new node and point to it
-        current -> pnext = malloc(sizeof(struct node));
+        current->pnext = malloc(sizeof(struct node));
         //point current to that new node
-        current = current -> pnext;
+        current = current->pnext;
         i++;
     }
 
     current = head;
     //print it
-    while(current -> pnext != NULL) {
-        printf("(%d)\n", current -> item);
-        current = current -> pnext;
+    while(current->pnext != NULL) {
+        printf("(%d)\n", current->item);
+        current = current->pnext;
     }
     printf("\n");//blank line for formatting
 
     //remove twice
     for(int i = 0; i < 2; i++) {
         struct node *old_head = head;
-        head = head -> pnext;
+        head = head->pnext;
         free(old_head);
     }
 
     //print again
     current = head;
-    while(current -> pnext != NULL) {
-        printf("(%d)\n", current -> item);
-        current = current -> pnext;
+    while(current->pnext != NULL) {
+        printf("(%d)\n", current->item);
+        current = current->pnext;
     }
 
     return 0;
